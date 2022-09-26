9ed81faeb734af56518be1781403bef860842c47
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2022-09-26 18:07:09 +0530

    Add files via upload

---
+++ Preprocessing and Linking error/Preprocessing and Linking error/InternalHeaderFile.h
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+void sayHi(){
+	
+	std::cout<<"\nHii..";
+}
---
+++ Preprocessing and Linking error/Preprocessing and Linking error/TestHeaderFile.cpp
@@ -0,0 +1,8 @@
+//TestHeaderFile.cpp
+//#include "HeaderFileTrial.h";
+
+void Test(){
+	std::cout<<"\nTest Called ..";
+	//sayHello();
+	std::cout<<"\nTest End..";
+}
